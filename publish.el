f0300335da96158c08858309e8dbdb4b647465ca
Justin Schechter <justin@nextdoorco.com>
2021-02-14 13:55:12 -0500

    Delete unused files

--- publish.el
+++
@@ -1,7 +0,0 @@
-(setq org-publish-project-alist
-      '(("keyboard"
-         :base-directory "~/notes/"
-         :exclude "org"
-         :include ("keyboard.org")
-         :publishing-function org-html-publish-to-html
-         :publishing-directory "~/public_html")))
